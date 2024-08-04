73145c43623f6059a40942b442b5b55049f4af50
EltonHOEvangelista <132697682+EltonHOEvangelista@users.noreply.github.com>
2024-08-03 23:42:23 -0700

    attempt 13

--- server/server.js
+++ server/server.js
@@ -134,5 +134,3 @@ app.use(express.static(path.join(__dirname, '../auto-mp-client/dist')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../auto-mp-client/dist', 'index.html'));
 });
-
-module.exports = app;
